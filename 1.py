75b446d09d3320bbce18fe0100b1c39faf3a7a0b
Albina3110 <albinahromcenkova614@gmail.com>
2024-11-02 10:38:23 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,24 @@
+class Car:
+    color 
+    fuel  
+    speed
+    material
+    probeg
+    def go (self):
+       pass 
+    def turn (self):
+       pass
+    def stop (self):
+        pass
+ myCar= Car
+ myCar.color "pink"
+ myCar.fuel=75
+ myCar.speed=120
+ myCar.material= "iron"
+ myCar.probeg=60
+ 
+ myCar.go (self)
+ myCar.turn(self)
+ myCar.stop(self)
+ print(myCar)
+    
